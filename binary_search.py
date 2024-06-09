552d02b2ff11dff5ad5223765b67cc23c1432607
Ibrahim <1b05h1n@pm.me>
2024-06-08 23:17:40 -0700

    Binary search

---
+++ binary_search.py
@@ -0,0 +1,29 @@
+## taken from grokking algorithms book
+
+def binary_search(list,item):
+    low = 0
+    high = len(list)-1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid + 1
+    return None
+
+my_list = [1,3,5,7,9]
+
+print(binary_search(my_list,3)) # 1
+print(binary_search(my_list,-1)) # None
+
+
+# Time complexity of binary search is O(log n), meaning time to complete algo increases logarithmically with the size of the input.
+
+# Exercise 1
+# 1.1 Suppose you have a sorted list of 128 names, and you're searching through it using binary search. What's the maximum number of steps it would take?
+# log2(128) = 7
+
+# 1.2 Suppose you double the size of the list. What's the maximum number of steps now?
+# log2(256) = 8
